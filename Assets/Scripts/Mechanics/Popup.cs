18519cb1cb2567dd6da61287588e346c0a8a72a7
seaerchin <jer005@e.ntu.edu.sg>
2020-10-14 21:47:52 +0800

    added popup

---
+++ Assets/Scripts/Mechanics/Popup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    public GameObject ui; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open() { 
+        ui.SetActive(true);
+    }
+
+    public void Close() { 
+        ui.SetActive(false);
+    }
+}
