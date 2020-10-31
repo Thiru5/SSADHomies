6ddadb58723afaaa208822c330bde6c9c7a5921e
seaerchin <jer005@e.ntu.edu.sg>
2020-10-31 22:18:45 +0800

    added dynamic text display on token collection

--- Assets/Scripts/Connections/DatabaseConnection.cs
+++ Assets/Scripts/Connections/DatabaseConnection.cs
@@ -9,6 +9,7 @@ public static class DatabaseConnection
 {
     private static readonly string DBAddr = "https://thequestofthessadhomies.firebaseio.com/";
     private static Dictionary<string, Dictionary<string, string>> questionBank;
+    private static Dictionary<string, string> textBank;
     // we need to fetch all questions from db and alloc
     public async static void init() { 
         // method fetches all questions/answer tuple from database
@@ -18,6 +19,16 @@ public static class DatabaseConnection
         var reader = new StreamReader(stream);
         questionBank = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(reader.ReadToEnd());
         response.Close();
+        var textReq = WebRequest.Create(DBAddr + "/tokenText.json"); // our structure here is dict(str: dict(str, str)) 
+        response = await textReq.GetResponseAsync();
+        stream = response.GetResponseStream();
+        reader = new StreamReader(stream);
+        textBank = JsonConvert.DeserializeObject<Dictionary<string, string>>(reader.ReadToEnd());
+    }
+
+    // use this to get a question - can change signature to be void -> string if we want to rack within this
+    public static string getText(int index) { 
+        return textBank[(index / textBank.Count).ToString()];
     }
 
     public static bool isValidUser(string matric, string password) {
