b7f39efd79a9e574111cb36778d06ad355004677
seaerchin <jer005@e.ntu.edu.sg>
2020-10-31 17:26:40 +0800

    token score added

---
+++ Assets/Scripts/Mechanics/ScoreController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Platformer.Mechanics;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField]
+    private TokenController controller;
+    public TMP_Text token_score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        token_score.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        token_score.text = controller.getCount.ToString();
+    }
+}
